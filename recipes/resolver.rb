39bafb5261c66ee4fe4a910296c98cf18a17f0c4
Cédric Félizard <cedric@felizard.fr>
2018-08-27 22:22:10 +0000

    Add Cloudflare DNS to resolv.conf

--- recipes/resolver.rb
+++ recipes/resolver.rb
@@ -1,7 +1,8 @@
-# https://en.wikipedia.org/wiki/OpenDNS
+# Cloudflare & OpenDNS
 node.default['resolver']['nameservers'] = %w(
+  1.1.1.1
   208.67.222.222
-  208.67.220.220
+  2606:4700:4700::1111
   2620:0:ccc::2
 )
 
