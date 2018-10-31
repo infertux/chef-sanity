eb69e824ddbd50261a556b688c7a3966fe0a122c
Cédric Félizard <cedric@felizard.fr>
2018-10-31 15:23:11 +0000

    Tweak Tmux config

--- recipes/tmux.rb
+++ recipes/tmux.rb
@@ -1,1 +1,15 @@
 package 'tmux'
+
+file '/etc/tmux.conf' do
+  owner 'root'
+  group 'root'
+  mode '0444'
+  content <<~CONF
+    # Use `tmux source-file /etc/tmux.conf` to reload this config
+
+    set -g prefix '`'
+    set -g visual-activity on
+    setw -g monitor-activity on
+    setw -g window-status-current-style bg=red
+  CONF
+end
