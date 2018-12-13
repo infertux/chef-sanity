8d0e7893e853671df8626e8899f337930cab34ed
Cédric Félizard <cedric@felizard.fr>
2018-12-13 15:01:03 +0000

    Remove any old config disabling IPv6

--- recipes/ipv6.rb
+++ recipes/ipv6.rb
@@ -1,12 +1,11 @@
-unless node['sanity']['ipv6']
-  require 'pathname'
+require 'pathname'
 
-  Pathname.glob('/proc/sys/net/ipv6/conf/*/disable_ipv6').map do |path|
-    interface = path.to_s.split('/')[-2]
+Pathname.glob('/proc/sys/net/ipv6/conf/*/disable_ipv6').map do |path|
+  interface = path.to_s.split('/')[-2]
 
-    sysctl "ipv6-#{interface}" do
-      key "net.ipv6.conf.#{interface}.disable_ipv6"
-      value 1
-    end
+  sysctl "ipv6-#{interface}" do
+    action node['sanity']['ipv6'] ? :remove : :apply
+    key "net.ipv6.conf.#{interface}.disable_ipv6"
+    value 1
   end
 end
