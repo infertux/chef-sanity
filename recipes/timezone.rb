f1a0f0e77d845026b60675275e91455c0cc6a1e9
Cédric Félizard <cedric@felizard.fr>
2020-01-27 18:37:39 +0000

    Fix broken NTP detection on Debian 10

--- recipes/timezone.rb
+++ recipes/timezone.rb
@@ -2,14 +2,17 @@
 
 case node['platform_family']
 when 'debian', 'rhel'
+  # XXX: `timedatectl show` is only available since Debian 10 so we fall back
+  # to `timedatectl status` otherwise
+
   execute 'set-timezone UTC' do
     command 'timedatectl set-timezone UTC'
-    not_if "timedatectl status | grep 'Time zone: UTC'"
+    not_if "timedatectl show 2>&1 | grep -qE '^Timezone=UTC$' || timedatectl status | grep -qE 'Time zone: UTC'"
   end
 
   execute 'set-ntp true' do
     command 'timedatectl set-ntp true'
-    not_if "timedatectl status | grep -E '(Network time on|systemd-timesyncd\.service active): yes'"
+    not_if "timedatectl show 2>&1 | grep -qE '^NTP=yes$' || timedatectl status | grep -qE 'Network time on: yes$'"
   end
 
 else
