50f5892659da70e105d0c1818c4febdc089e0732
Cédric Félizard <cedric@felizard.fr>
2020-04-16 09:14:11 +0000

    Keep old config upon updating package with conflict
    E.g. "Package telegraf has conffile prompt and needs to be upgraded manually"

--- recipes/unattended_upgrades.rb
+++ recipes/unattended_upgrades.rb
@@ -8,6 +8,7 @@ when 'debian'
   node.default['apt']['unattended_upgrades']['sender'] ||= node['sanity']['root_email']
   node.default['apt']['unattended_upgrades']['remove_unused_dependencies'] = true
   node.default['apt']['unattended_upgrades']['automatic_reboot'] = node['sanity']['automatic_reboot']
+  node.default['apt']['unattended_upgrades']['dpkg_options'] = ['--force-confdef', '--force-confold'] # https://askubuntu.com/questions/921162/how-can-i-automate-a-conffile-prompt-in-unattended-upgrades
 
   include_recipe 'apt::unattended-upgrades'
 else
