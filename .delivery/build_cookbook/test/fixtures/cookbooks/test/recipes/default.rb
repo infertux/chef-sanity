3c6e0bace296734bdabf523da7de92f1faa48a30
Cédric Félizard <cedric@felizard.fr>
2020-06-23 12:58:33 +0000

    WIP

--- .delivery/build_cookbook/test/fixtures/cookbooks/test/recipes/default.rb
+++ .delivery/build_cookbook/test/fixtures/cookbooks/test/recipes/default.rb
@@ -1,5 +1,4 @@
-%w(unit lint syntax).each do |phase|
-  # TODO: This works on Linux/Unix. Not Windows.
+%w(lint syntax smoke).each do |phase|
   execute "HOME=/home/vagrant delivery job verify #{phase} --server localhost --ent test --org kitchen" do
     cwd '/tmp/repo-data'
     user 'vagrant'
