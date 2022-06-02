a6e1cb04fbf954ae00932336ecb6e10c8306e7d8
Cédric Félizard <cedric@felizard.fr>
2022-06-02 12:08:37 +0000

    Enable automatic SMART checks

---
+++ recipes/smart.rb
@@ -0,0 +1,24 @@
+package 'smartmontools'
+
+service 'smartmontools' do
+  action %i(enable start)
+end
+
+file '/etc/smartd.conf' do
+  owner 'root'
+  group 'root'
+  mode '0444'
+  notifies :restart, 'service[smartmontools]'
+  content [
+    'DEVICESCAN -a', # monitor all attributes
+    "-m #{node['sanity']['root_email']}", # send warning emails to this address
+    '-M exec /usr/share/smartmontools/smartd-runner', # use helper script shipped with https://packages.debian.org/bullseye/amd64/smartmontools/filelist
+    '-M test', # send additional test email when smartd starts up
+    '-S on', # enables Attribute Autosave when smartd starts up
+    '-W 5,40,45', # log changes of 5+ degrees, or when temp over 40, and warns when over 45
+    '-d removable', # continue if the device does not appear to be present when smartd is started
+    '-n standby,q', # don't spin up disk when in sleep or standby mode
+    '-o on', # enables SMART Automatic Offline Testing when smartd starts up
+    '-s (S/../.././02|L/../../6/03)', # short self-test every day between 2-3am, and an extended self test weekly on Saturdays between 3-4am
+  ].join(' ')
+end
