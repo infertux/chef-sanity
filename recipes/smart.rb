5c970d9159dd8ff1429d1e344fb997a750d9775e
Cédric Félizard <cedric@felizard.fr>
2022-08-25 02:22:46 +0000

    Increase SMART temperature warnings to avoid false positive spam

--- recipes/smart.rb
+++ recipes/smart.rb
@@ -13,9 +13,8 @@ file '/etc/smartd.conf' do
     'DEVICESCAN -a', # monitor all attributes
     "-m #{node['sanity']['root_email']}", # send warning emails to this address
     '-M exec /usr/share/smartmontools/smartd-runner', # use helper script shipped with https://packages.debian.org/bullseye/amd64/smartmontools/filelist
-    '-M test', # send additional test email when smartd starts up
     '-S on', # enables Attribute Autosave when smartd starts up
-    '-W 5,40,45', # log changes of 5+ degrees, or when temp over 40, and warns when over 45
+    '-W 5,45,50', # log changes of 5+ degrees, or when temp over 45, and warns when over 50 (typical manufacturer max temp is 60)
     '-d removable', # continue if the device does not appear to be present when smartd is started
     '-n standby,q', # don't spin up disk when in sleep or standby mode
     '-o on', # enables SMART Automatic Offline Testing when smartd starts up
