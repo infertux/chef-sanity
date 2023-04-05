fcf3c4e5a9f46f79260b86beb627b3ddae5bcf6e
Cédric Félizard <cedric@felizard.fr>
2023-04-05 08:33:45 +0000

    Don't remove nfs-common and rpcbind as they are useful in some cases

--- recipes/packages.rb
+++ recipes/packages.rb
@@ -1,6 +1,6 @@
 # Remove unwanted packages often installed by default
 unwanted_packages = value_for_platform_family(
-  %w(debian) => %w(nfs-common rpcbind bind9 sysstat),
+  %w(debian) => %w(bind9 sysstat),
   %w(rhel) => %w(centos-logos),
 )
 
