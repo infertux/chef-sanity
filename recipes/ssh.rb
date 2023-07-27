f65bd462997bdb57140cc9161a0d3966f1801ecd
Cédric Félizard <cedric@felizard.fr>
2023-07-27 09:36:56 +0000

    Harden sshd config and disable RSA host key

--- recipes/ssh.rb
+++ recipes/ssh.rb
@@ -25,12 +25,14 @@ node.default['ssh-hardening']['network']['ipv6']['enable'] = node['sanity']['ipv
 
 %w(client server).each do |component|
   # recommended crypto from https://github.com/dev-sec/ssh-baseline
-  node.default['ssh-hardening']['ssh'][component]['cipher'] = 'aes256-ctr,aes192-ctr,aes128-ctr'
+  # and https://sshcheck.com/
+  node.default['ssh-hardening']['ssh'][component]['cipher'] = 'aes256-ctr'
   node.default['ssh-hardening']['ssh'][component]['kex'] = 'curve25519-sha256,diffie-hellman-group-exchange-sha256'
-  node.default['ssh-hardening']['ssh'][component]['mac'] = 'hmac-sha2-512,hmac-sha2-256'
+  node.default['ssh-hardening']['ssh'][component]['mac'] = 'hmac-sha2-512'
 end
 
 node.default['ssh-hardening']['ssh']['server']['allow_root_with_key'] = true
+node.default['ssh-hardening']['ssh']['server']['host_key_files'] = %w(/etc/ssh/ssh_host_ed25519_key)
 node.default['ssh-hardening']['ssh']['use_privilege_separation'] = 'sandbox'
 
 include_recipe 'ssh-hardening::default'
