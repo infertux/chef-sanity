95512b03b619d5015d055d4f934fe4d54531a5d3
Cédric Félizard <cedric@felizard.fr>
2024-01-08 03:46:16 +0000

    Prefer IPv6 DNS over IPv4

--- attributes/default.rb
+++ attributes/default.rb
@@ -55,7 +55,7 @@ default['sanity']['repositories']['protocol'] = 'https' # http|https
 
 # Use Cloudflare & OpenDNS by default
 default['sanity']['resolver']['dns'] = %w(1.1.1.1 208.67.222.222)
-default['sanity']['resolver']['dns'] |= %w(2606:4700:4700::1111 2620:119:35::35) if node['sanity']['ipv6']
+default['sanity']['resolver']['dns'].unshift(*%w(2606:4700:4700::1111 2620:119:35::35)) if node['sanity']['ipv6']
 default['sanity']['resolver']['dnssec'] = true
 
 default['sanity']['root_email'] = '' # administrator's email
