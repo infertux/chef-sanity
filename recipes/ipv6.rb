d3cc786e6044c09ce43d9525f26acfe7d51cb2d8
Cédric Félizard <cedric@felizard.fr>
2025-09-07 01:50:28 +0000

    Always set net.ipv6.conf.all.disable_ipv6 value
    This avoids conflicting with the os-hardening cookbook.

--- recipes/ipv6.rb
+++ recipes/ipv6.rb
@@ -3,9 +3,16 @@ require 'pathname'
 Pathname.glob('/proc/sys/net/ipv6/conf/*/disable_ipv6').map do |path|
   interface = path.to_s.split('/')[-2]
 
-  sysctl "ipv6-#{interface}" do
-    action node['sanity']['ipv6'] ? :remove : :apply
-    key "net.ipv6.conf.#{interface}.disable_ipv6"
-    value 1
+  if interface == 'all'
+    sysctl "ipv6-#{interface}" do
+      key "net.ipv6.conf.#{interface}.disable_ipv6"
+      value node['sanity']['ipv6'] ? 0 : 1
+    end
+  else
+    sysctl "ipv6-#{interface}" do
+      action node['sanity']['ipv6'] ? :remove : :apply
+      key "net.ipv6.conf.#{interface}.disable_ipv6"
+      value 1
+    end
   end
 end
