5b13f3070cfe150b1447ca28ef9dc9a1ebc76c43
Cédric Félizard <cedric@felizard.fr>
2025-12-18 01:22:22 +0000

    Drop support for legacy iptables

--- metadata.rb
+++ metadata.rb
@@ -6,16 +6,15 @@ description      'Standardize configuration of various machines (VM, cloud, bare
 source_url       'https://github.com/infertux/chef-sanity'
 issues_url       'https://github.com/infertux/chef-sanity/issues'
 
-version          '3.13.2'
+version          '3.14.0'
 chef_version     '>= 18'
 
 supports 'debian', '>= 12.0'
 supports 'ubuntu', '>= 24.04'
 
-depends 'apt', '~> 7.5'
-depends 'iptables-ng', '~> 4.1'
+depends 'apt', '~> 7.7'
 depends 'monit-ng', '~> 2.4'
 depends 'os-hardening', '~> 4.2'
-depends 'postfix', '~> 6.0'
+depends 'postfix', '~> 6.4'
 depends 'resolver', '~> 4.2'
 depends 'ssh-hardening', '~> 2.9'
